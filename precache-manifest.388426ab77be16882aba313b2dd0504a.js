bf320005b021d06766abd8d7b29ede436569d35c
Βασίλειος Κουτσούκης <98vasilis98@gmail.com>
2021-05-20 13:00:51 +0300

    Updates

---
+++ precache-manifest.388426ab77be16882aba313b2dd0504a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bd5e5d99da084a8cc1f46139fe08621",
+    "url": "/Auction-app/index.html"
+  },
+  {
+    "revision": "fd24a1f62cfbc730985c",
+    "url": "/Auction-app/static/css/main.63fc24eb.chunk.css"
+  },
+  {
+    "revision": "0311af9ea4bd96e5415f",
+    "url": "/Auction-app/static/js/2.64428b2b.chunk.js"
+  },
+  {
+    "revision": "fd24a1f62cfbc730985c",
+    "url": "/Auction-app/static/js/main.7c8881b8.chunk.js"
+  },
+  {
+    "revision": "f7472452af8151a2b3cc",
+    "url": "/Auction-app/static/js/runtime-main.0409cdc2.js"
+  }
+]);
